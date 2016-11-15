fb191e5e63e817e7d096650cdbbe65f3c58c1e99
Paul Patterson <pazerp@hotmail.com>
2016-11-15 22:34:03 +0000

    Introduced guard statement where reasonable.

--- BankManager/Shift.swift
+++ BankManager/Shift.swift
@@ -57,41 +57,43 @@ public final class Shift: ManagedObject, ManagedObjectType {
         paidTime = totalTime - unpaidTime
         
         let currentRatesPredicate = NSPredicate(format: "effectiveFrom <= %@ AND effectiveUntil == nil", start)
-        let currentPayStructure = PayStructure.findOrFetchInContext(managedObjectContext!, matchingPredicate: currentRatesPredicate)
-        let currentPayRate = HourlyRate.findOrFetchInContext(managedObjectContext!, matchingPredicate: currentRatesPredicate)
-        let currentHolidayRate = HolidayRate.findOrFetchInContext(managedObjectContext!, matchingPredicate: currentRatesPredicate)
+        let errorMsg = "Failed to extract %@ with predicate %@"
         
-        if let payStructure = currentPayStructure, payRate = currentPayRate, holidayRate = currentHolidayRate {
-            let shiftComps = payStructure.componentsForShiftStarting(start, ending: end)
-            
-            // Work out what we'll be subtracting as a result of the unpaid break
-            var remainingUnpaidTime = unpaidTime
-            var rateAdjustedUnpaidTime: NSTimeInterval = 0
-            for component in shiftComps.sort ( { $0.duration > $1.duration } ) {
-                let time = min(remainingUnpaidTime, component.duration)
-                rateAdjustedUnpaidTime += time * component.rate
-                remainingUnpaidTime -= time
-            }
-            
-            accruedBasicHours = shiftComps.reduce(0, combine: { $0 + ($1.duration * $1.rate) } ) - rateAdjustedUnpaidTime
-            grossPay = accruedBasicHours.inHours * payRate.rate
-            holidayTime = accruedBasicHours * holidayRate.rate
-            holidayPay = holidayTime.inHours * payRate.rate
-        } else {
-            NSLog("failed to find pay structure matching predicate %@", currentRatesPredicate)
+        guard let payStructure = PayStructure.findOrFetchInContext(managedObjectContext!, matchingPredicate: currentRatesPredicate) else {
+            NSLog(errorMsg, "PayStructure"); return
         }
+        
+        guard let payRate = HourlyRate.findOrFetchInContext(managedObjectContext!, matchingPredicate: currentRatesPredicate) else {
+            NSLog(errorMsg, "HourlyRate"); return
+        }
+        
+        guard let holidayRate = HolidayRate.findOrFetchInContext(managedObjectContext!, matchingPredicate: currentRatesPredicate) else {
+            NSLog(errorMsg, "HolidayRate"); return
+        }
+        
+        let shiftComps = payStructure.componentsForShiftStarting(start, ending: end)
+        
+        // Work out what we'll be subtracting as a result of the unpaid break
+        var remainingUnpaidTime = unpaidTime
+        var rateAdjustedUnpaidTime: NSTimeInterval = 0
+        for component in shiftComps.sort ( { $0.duration > $1.duration } ) {
+            let time = min(remainingUnpaidTime, component.duration)
+            rateAdjustedUnpaidTime += time * component.rate
+            remainingUnpaidTime -= time
+        }
+        
+        accruedBasicHours = shiftComps.reduce(0, combine: { $0 + ($1.duration * $1.rate) } ) - rateAdjustedUnpaidTime
+        grossPay = accruedBasicHours.inHours * payRate.rate
+        holidayTime = accruedBasicHours * holidayRate.rate
+        holidayPay = holidayTime.inHours * payRate.rate
     }
     
     public static func insertIntoContext(moc: NSManagedObjectContext, csvString: String) throws -> Shift {
         
-        
-        // First parse the csv string
         var shift: Shift!
-        var startDate: NSDate!
-        var stopDate: NSDate!
 
         let rawValues = csvString.componentsSeparatedByString(",")
-        if rawValues.count != 4 {
+        guard rawValues.count == 4 else {
             throw ShiftParsingError.unexpectedCSVFormat(offendingString: csvString)
         }
         
@@ -99,19 +101,17 @@ public final class Shift: ManagedObject, ManagedObjectType {
         let rawTimes = rawValues[3]
         let timeComps = rawTimes.componentsSeparatedByString("-")
         
-        if timeComps.count != 2 {
+        guard timeComps.count == 2 else {
             throw ShiftParsingError.unexpectedTimesFormat(offendingString: rawTimes)
         }
         
         let rawStartDate = "\(rawDate), \(timeComps.first!)"
-        startDate = Shift.dateFormatter.dateFromString(rawStartDate)
-        if startDate == nil {
+        guard let startDate = Shift.dateFormatter.dateFromString(rawStartDate) else {
             throw ShiftParsingError.unexpectedDateFormat(offendingString: rawStartDate)
         }
         
         let provisialRawStopDate = "\(rawDate), \(timeComps.last!)"
-        stopDate = Shift.dateFormatter.dateFromString(provisialRawStopDate)
-        if stopDate == nil {
+        guard var stopDate = Shift.dateFormatter.dateFromString(provisialRawStopDate) else {
             throw ShiftParsingError.unexpectedDateFormat(offendingString: provisialRawStopDate)
         }
         
@@ -123,16 +123,16 @@ public final class Shift: ManagedObject, ManagedObjectType {
         let siteName = rawValues[1].stringByTrimmingCharactersInSet(whitespaceAndNewlineCharSet)
         let wardName = rawValues[2].stringByTrimmingCharactersInSet(whitespaceAndNewlineCharSet)
         
-        if let site = Site.siteWithName(siteName, inManagedObjectContext: moc) {
-            let ward = site.wardWithName(wardName, insertIfNecessary: true)!
-            shift = Shift.insertIntoContext(moc)
-            shift.start = startDate
-            shift.end = stopDate
-            shift.ward = ward
-            shift.updateTimeAndMoney()
-        } else {
+        guard let site = Site.siteWithName(siteName, inManagedObjectContext: moc) else {
             throw ShiftInsertionError.unrecognisedSiteError(siteName: siteName)
         }
+        
+        let ward = site.wardWithName(wardName, insertIfNecessary: true)!
+        shift = Shift.insertIntoContext(moc)
+        shift.start = startDate
+        shift.end = stopDate
+        shift.ward = ward
+        shift.updateTimeAndMoney()
         
         return shift
         
